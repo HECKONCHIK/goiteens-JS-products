bf592efde9d8872ea180036f16fa593878017b04
Oleh Tsap <olehtsap07@gmail.com>
2023-05-16 21:28:49 +0300

    продовження практики

--- src/js/index.js
+++ src/js/index.js
@@ -9,9 +9,14 @@ const ref = {
 
 
 async function getProduct() {
-    const res = await fetch('http://localhost:3000/products');
+    try {
+            const res = await fetch('http://localhost:3000/products');
     const products = await res.json();
     return products
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
 function makeMarkup(products) {
@@ -24,6 +29,36 @@ function makeMarkup(products) {
     })
 }
 
-const markup = makeMarkup(getProduct);
+getProduct().then(products => {
+    const markup = makeMarkup(products);
 
-ref.product.insertAdjacentElement("beforeend", ...markup);
+    ref.product.insertAdjacentHTML("beforeend", markup);
+})
+
+
+
+
+
+
+// 3. Додайте обробники подій до кожного товару, щоб при кліку на нього
+// відкривалася сторінка з детальною інформацією про товар.
+// На цій сторінці ви можете відобразити всю додаткову інформацію
+// про товар, таку як опис, ціна, зображення тощо.
+
+
+
+
+// 4. Для реалізації функціональності додавання товарів до кошика 
+// використовуйте JavaScript. Додайте кнопку "Додати до кошика" 
+// на сторінці детальної інформації про товар. При кліку на цю 
+// кнопку ви можете зберігати вибраний товар у змінній або масиві, 
+// які представляють кошик. Відображайте вибрані товари кошика разом з 
+// їхніми назвами та цінами на сторінці кошика.
+
+
+
+
+// 5. Додайте форму оформлення замовлення на сторінку кошика. 
+// Ця форма повинна містити поля для введення інформації про 
+// замовлення, такої як ім'я, електронна адреса, адреса доставки тощо. 
+// Додайте кнопку "Оформити замовлення", яка буде відправляти дані з форми на сервер для обробки.
